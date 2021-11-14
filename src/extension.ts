f5392ad51a02f658d872980de398a7d1d5624071
jamesinaxx <61040004+jamesinaxx@users.noreply.github.com>
2021-11-14 12:46:47 +1100

    Fixes

--- src/extension.ts
+++ src/extension.ts
@@ -1,26 +1,40 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
+import sort from 'sort-package-json';
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
+const sortPackageJson = async (doc: vscode.TextDocument) => {
+  if (!doc.fileName.endsWith('package.json')) {
+    return;
+  }
+
+  const text = doc.getText();
+
+  const sorted = sort(text);
+
+  await vscode.workspace.fs.writeFile(doc.uri, Buffer.from(sorted, 'utf8'));
+};
+
 export function activate(context: vscode.ExtensionContext) {
-	
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "sort-package" is now active!');
+  console.log('Congratulations, your extension "sort-package" is now active!');
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('sort-package.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from Sort Package!');
-	});
+  context.subscriptions.push(
+    vscode.commands.registerCommand('sort-package.sortPackage', async () => {
+      const editor = vscode.window.activeTextEditor;
+      if (!editor) {
+        return;
+      }
 
-	context.subscriptions.push(disposable);
+      await sortPackageJson(editor.document);
+    }),
+  );
+
+  context.subscriptions.push(
+    vscode.workspace.onDidSaveTextDocument(async doc => {
+      const config = vscode.workspace.getConfiguration('sortPackage');
+      if (config.get<boolean>('onSave')) {
+        await sortPackageJson(doc);
+      }
+    }),
+  );
 }
 
-// this method is called when your extension is deactivated
 export function deactivate() {}
